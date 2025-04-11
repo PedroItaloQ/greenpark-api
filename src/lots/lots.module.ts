86e89c6aad92f0c1f126873b68568ee3d1f8ff8e
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 15:33:49 -0300

    feat: created lots module

--- src/lots/lots.module.ts
+++ src/lots/lots.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LotsService } from './lots.service';
+import { LotsController } from './lots.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lot } from './entitie/lot.entity';
 
 @Module({
-  providers: [LotsService]
+  imports: [TypeOrmModule.forFeature([Lot])],
+  providers: [LotsService],
+  controllers: [LotsController],
+  exports: [LotsService],
 })
 export class LotsModule {}
