6656e940474a07b6ce524f0f2325c4acae4007b0
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 19:48:32 -0300

    feat: created import controller and module

--- src/imports/imports.controller.ts
+++ src/imports/imports.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ImportsService } from './imports.service';
+import { CreateImportDto } from './dto/create-import.dto';
 
-@Controller('imports')
-export class ImportsController {}
+@Controller('importacoes')
+export class ImportsController {
+  constructor(private readonly service: ImportsService) {}
+
+  @Post()
+  create(@Body() dto: CreateImportDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+}
--- src/imports/imports.module.ts
+++ src/imports/imports.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImportsService } from './imports.service';
+import { ImportsController } from './imports.controller';
+import { Import } from './entitie/import.entity';
 
 @Module({
-  providers: [ImportsService]
+  imports: [TypeOrmModule.forFeature([Import])],
+  controllers: [ImportsController],
+  providers: [ImportsService],
+  exports: [ImportsService],
 })
 export class ImportsModule {}
