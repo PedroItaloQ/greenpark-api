d17f95c7b5ab1e3f5f23c8298a287a275310b475
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 13:45:42 -0300

    feat: created lot interface

---
+++ src/lots/entitie/lot.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { Invoice } from '../../invoices/entitie/invoice.entity';
+
+@Entity('lotes')
+export class Lot {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ name: 'nome', length: 100 })
+  name: string;
+
+  @Column({ name: 'ativo', default: true })
+  active: boolean;
+
+  @CreateDateColumn({ name: 'criado_em' })
+  created_at: Date;
+
+}
