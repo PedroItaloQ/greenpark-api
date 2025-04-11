9e087289a15197245278986356caf9b65aecb9e8
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 15:46:26 -0300

    feat: created import service

--- src/imports/imports.service.ts
+++ src/imports/imports.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Import } from './entitie/import.entity';
+import { CreateImportDto } from './dto/create-import.dto';
 
 @Injectable()
-export class ImportsService {}
+export class ImportsService {
+  constructor(
+    @InjectRepository(Import)
+    private importRepo: Repository<Import>,
+  ) {}
+
+  async create(dto: CreateImportDto): Promise<Import> {
+    const imp = this.importRepo.create(dto);
+    return this.importRepo.save(imp);
+  }
+
+  async findAll(): Promise<Import[]> {
+    return this.importRepo.find();
+  }
+
+  async findOne(id: string): Promise<Import> {
+    const imp = await this.importRepo.findOneBy({ id });
+    if (!imp) {
+      throw new NotFoundException(`Importação com ID ${id} não encontrada`);
+    }
+    return imp;
+  }
+}
