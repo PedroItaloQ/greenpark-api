5a1a288d8e7cd0455becfe996f56b5a353dac6b4
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 15:33:59 -0300

    feat: created lots service

--- src/lots/lots.service.ts
+++ src/lots/lots.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lot } from './entitie/lot.entity';
+import { Repository } from 'typeorm';
+import { CreateLotDto } from './dto/create-lot.dto';
+import { UpdateLotDto } from './dto/update-lot.dto';
 
 @Injectable()
-export class LotsService {}
+export class LotsService {
+  constructor(
+    @InjectRepository(Lot)
+    private lotRepo: Repository<Lot>,
+  ) {}
+
+  async create(dto: CreateLotDto): Promise<Lot> {
+    const lot = this.lotRepo.create(dto);
+    return this.lotRepo.save(lot);
+  }
+
+  async findAll(): Promise<Lot[]> {
+    return this.lotRepo.find();
+  }
+
+  async findOne(id: string): Promise<Lot> {
+    const lot = await this.lotRepo.findOneBy({ id });
+    if (!lot) throw new NotFoundException('Lote não encontrado');
+    return lot;
+  }
+
+  async update(id: string, dto: UpdateLotDto): Promise<Lot> {
+    const lot = await this.findOne(id);
+    Object.assign(lot, dto);
+    return this.lotRepo.save(lot);
+  }
+
+  async remove(id: string): Promise<void> {
+    const lot = await this.findOne(id);
+    await this.lotRepo.remove(lot);
+  }
+}
