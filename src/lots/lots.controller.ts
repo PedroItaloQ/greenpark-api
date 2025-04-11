6805bb756c25d11bc1339b55a8ba261e3d9d3dcb
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 15:33:23 -0300

    feat: created lotsController

--- src/lots/lots.controller.ts
+++ src/lots/lots.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { LotsService } from './lots.service';
+import { CreateLotDto } from './dto/create-lot.dto';
+import { UpdateLotDto } from './dto/update-lot.dto';
 
-@Controller('lots')
-export class LotsController {}
+@Controller('lotes')
+export class LotsController {
+  constructor(private readonly service: LotsService) {}
+
+  @Post()
+  create(@Body() dto: CreateLotDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateLotDto) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.remove(id);
+  }
+}
