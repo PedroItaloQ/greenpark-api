14ee6d11841fd00e7d0543ad2cdfcd452b24e8aa
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-04-11 21:07:39 -0300

    feat: added import in module

--- src/imports/imports.module.ts
+++ src/imports/imports.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImportsService } from './imports.service';
 import { ImportsController } from './imports.controller';
 import { Import } from './entitie/import.entity';
+import { Invoice } from 'src/invoices/entitie/invoice.entity';
+import { Lot } from 'src/lots/entitie/lot.entity';
+import { CSVImportController } from '../imports/csv-import.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Import])],
-  controllers: [ImportsController],
+  imports: [
+    TypeOrmModule.forFeature([Import, Invoice, Lot])
+  ],
+  controllers: [ImportsController, CSVImportController],
   providers: [ImportsService],
   exports: [ImportsService],
 })
